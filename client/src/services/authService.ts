9ba3821a4e65a1092e1e22eeebb22a56e207b9e3
Garvit Arora <garvit.university@gmail.com>
2025-09-19 17:45:25 +0530

    updating

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-// const API_BASE_URL =
-//   process.env.REACT_APP_API_URL || "http://localhost:5000/api";
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+// const API_BASE_URL = "http://localhost:3000";
+
+
 
 const authService = {
   register: async (userData: {
