363f4f4c213d73265297f7c48f157d5f7041eb5f
Garvit Arora <garvit.university@gmail.com>
2025-09-19 13:39:51 +0530

    refator: change api_base_url vars

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+// const API_BASE_URL =
+//   process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = "http://localhost:3000";
 
 const authService = {
   register: async (userData: {
